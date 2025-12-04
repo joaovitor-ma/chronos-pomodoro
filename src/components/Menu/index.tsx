90b2d7227fb576452492346d0b6995c02ea2ce44
João Vitor <jvitorcavalcante18@gmail.com>
2025-12-04 18:09:12 -0300

    Iniciando função de alternar entre modo claro e escuro

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,19 +1,51 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react"
-import styles from "./styles.module.css"   
+import styles from "./styles.module.css"
+import { useState } from "react"
+
+type availableThemes = "dark" | "light"
 
 export function Menu() {
+
+    const [theme, setTheme] = useState<availableThemes>("dark")
+
+    function handleThemeChange(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+        e.preventDefault()
+        setTheme(currentTheme => currentTheme == "dark" ? "light" : "dark")
+    }
+
     return (
         <nav className={styles.menu}>
-            <a className={styles.menuLink} href="#">
+            <a
+                className={styles.menuLink}
+                href="#"
+                aria-label="Ir para a Home"
+                title="Ir para a home"
+            >
                 <HouseIcon />
             </a>
-            <a className={styles.menuLink} href="#">
+            <a
+                className={styles.menuLink}
+                href="#"
+                aria-label="Ver histórico"
+                title="Ver histórico"
+            >
                 <HistoryIcon />
             </a>
-            <a className={styles.menuLink} href="#">
+            <a
+                className={styles.menuLink}
+                href="#"
+                aria-label="Configurações"
+                title="Configurações"
+            >
                 <SettingsIcon />
             </a>
-            <a className={styles.menuLink} href="#">
+            <a
+                className={styles.menuLink}
+                href="#"
+                aria-label="Mudar tema"
+                title="Mudar tema"
+                onClick={handleThemeChange}
+            >
                 <SunIcon />
             </a>
         </nav>
