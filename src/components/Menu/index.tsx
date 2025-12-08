4cba764244c1cc4d3b14f0b40fdcbe010e32a170
João Vitor <jvitorcavalcante18@gmail.com>
2025-12-08 10:13:55 -0300

    Finalizando função para alternar entre tema claro e escuro.

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,17 +1,30 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react"
+import { HistoryIcon, HouseIcon, MoonIcon, SettingsIcon, SunIcon } from "lucide-react"
 import styles from "./styles.module.css"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
-type availableThemes = "dark" | "light"
+type AvailableThemes = "dark" | "light"
 
 export function Menu() {
 
-    const [theme, setTheme] = useState<availableThemes>("dark")
+    const [theme, setTheme] = useState<AvailableThemes>(() => {
+        const storageTheme = localStorage.getItem("theme") as AvailableThemes || "dark"
+        return storageTheme
+    })
+
+    const nextThemeIcon = {
+        dark: <SunIcon />,
+        light: <MoonIcon />
+    }
 
     function handleThemeChange(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
         e.preventDefault()
         setTheme(currentTheme => currentTheme == "dark" ? "light" : "dark")
     }
+
+    useEffect(() => {
+        document.documentElement.setAttribute("data-theme", theme)
+        localStorage.setItem("theme", theme)
+    }, [theme])
 
     return (
         <nav className={styles.menu}>
@@ -46,7 +59,7 @@ export function Menu() {
                 title="Mudar tema"
                 onClick={handleThemeChange}
             >
-                <SunIcon />
+                {nextThemeIcon[theme]}
             </a>
         </nav>
     )
