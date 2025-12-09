d5a384d5871fdc03751c4d3025b2551bcfe22396
João Vitor <jvitorcavalcante18@gmail.com>
2025-12-09 15:52:49 -0300

    Iniciando criação de funções de criação de tasks

--- src/components/CountDown/index.tsx
+++ src/components/CountDown/index.tsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.css"
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext"
 
 export function CountDown() {
-    const { state } = useTaskContext()
+    const { state, } = useTaskContext()
 
     return <div className={styles.container}>{state.formattedSecondsRemaining}</div>
 }
--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,12 +1,63 @@
+import { useRef } from "react";
+
 import { DefaultButton } from "../DefaultButton";
 import { Cycles } from "../Cycles";
 import { DefaultInput } from "../DefaultInput";
 
+import type { TaskModel } from "../../models/TaskModel";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext"
+
 import { PlayCircleIcon } from "lucide-react";
 
 export function MainForm() {
+
+    const { setState } = useTaskContext()
+
+    const taskNameRef = useRef<HTMLInputElement>(null)
+
+    function handleCreateNewTask(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+
+        if(!taskNameRef.current) return
+
+        const taskName = taskNameRef.current.value.trim()
+
+        if(!taskName) {
+            alert("Digite o nome da tarefa")
+            return
+        }
+
+        const newTask: TaskModel = {
+            id: Date.now().toString(),
+            name: taskName,
+            startDate: Date.now(),
+            completeDate: null,
+            interruptDate: null,
+            duration: 1,
+            type: "workTime"
+        }
+
+        const secondsRemaining = newTask.duration * 60
+
+        setState(prevState => {
+            return {
+                ...prevState,
+                activeTask: newTask,
+                currentCycle: 1,
+                secondsRemaining,
+                formattedSecondsRemaining: "00:00",
+                tasks: [
+                    ...prevState.tasks, newTask
+                ],
+                config: {
+                    ...prevState.config
+                }
+            }
+        })
+    }
+
     return (
-        <form action="" className="form">
+        <form onSubmit={handleCreateNewTask} action="" className="form">
             <div className="formRow">
                 <DefaultInput
                     id="meuInput"
@@ -14,6 +65,7 @@ export function MainForm() {
                     labelText="Task"
                     placeholder="Digite algo"
                     defaultValue={"valor preenchido"}
+                    ref={taskNameRef}
                 />
             </div>
 
