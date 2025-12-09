e99c9281e915379ad3150b3ce5ebd7177db29268
João Vitor <jvitorcavalcante18@gmail.com>
2025-12-09 10:54:13 -0300

    Usando context API para permitir manipulação e acesso de estado em múltiplos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Home } from "./pages/Home"
-import { NotFound } from "./pages/NotFound"
-import { AboutPomodoro } from "./pages/AboutPomodoro"
 
 import "./styles/theme.css"
 import "./styles/global.css"
 
+import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider"
+
 export function App() {
-    return <Home />
+    return <TaskContextProvider>
+        <Home />
+    </TaskContextProvider>
 }
