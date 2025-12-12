b568202567dd750ad861c1e79c1373e794bdb514
João Vitor <jvitorcavalcante18@gmail.com>
2025-12-12 11:32:35 -0300

    Criando web worker para fazer a contagem regressiva

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -2,6 +2,7 @@ import { useEffect, useReducer } from "react"
 import { initialTaskState } from "./initialTaskState"
 import { TaskContext } from "./TaskContext"
 import { taskReducer } from "./taskReducer"
+import { TimerWorkerManager } from "../../workers/TimerWorkerManager"
 
 type TaskContextProviderProps = {
     children: React.ReactNode
@@ -11,9 +12,27 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
 
     const [state, dispatch] = useReducer(taskReducer, initialTaskState)
 
+    const worker = TimerWorkerManager.getInstance()
+
+    worker.onmessage(e => {
+        const countDownSeconds = e.data
+        
+        console.log(countDownSeconds)
+
+        if(countDownSeconds <= 0) {
+            console.log("worker completed")
+            worker.terminate()
+        }
+    })
+
     useEffect(() => {
-        console.log(state)
-    }, [state])
+        if(!state.activeTask) {
+            console.log("worker terminado por falta de active task")
+            worker.terminate()
+        }
+
+        worker.postMessage(state)
+    }, [worker, state])
 
     return <TaskContext.Provider value={{ state, dispatch }}>
         {children}
